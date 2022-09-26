d73e9a3919d6c3fc9b29a5e6948d344b1f24952c
choxxy <jorh.choxxy@gmail.com>
2022-09-26 13:12:28 +0300

    Removed unused fragment

--- app/src/main/java/com/apolloagriculture/android/takehomeassignment/HomeFragment.kt
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/HomeFragment.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.apolloagriculture.android.takehomeassignment.data.response.Forecast
 import com.apolloagriculture.android.takehomeassignment.data.response.Weather
 import com.apolloagriculture.android.takehomeassignment.databinding.FragmentSecondBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
     private val weatherViewModel: WeatherViewModel by viewModel()
